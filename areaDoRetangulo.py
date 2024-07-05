ca43b7268deed86941136f8e51d53d4216fcdc50
meuNobre <miguelnobre0411@gmail.com>
2024-07-05 09:32:54 -0300

    Primeira Prova de Lógica

---
+++ areaDoRetangulo.py
@@ -0,0 +1,8 @@
+print(f"Bem-vindo(a) à calculadora de área de retângulos. Digite a seguir as informações solicitadas. Siga como base a fórmula:\nÁrea = Base x Altura")
+
+base = float(input("Digite a base do seu retângulo em cm (centímetros): "))
+altura = float(input("Digite a altura do seu retângulo em cm (centímetros): "))
+    
+area = base * altura
+
+print(f"A área do retângulo é {area}cm")
