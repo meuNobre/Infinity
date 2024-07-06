cc4b2fbb3817e24187f5b3ebeee04605384d66db
meuNobre <153314662+meuNobre@users.noreply.github.com>
2024-07-06 19:34:05 -0300

    Segunda Prova - Condicionais

--- numeros.py
+++ numeros.py
@@ -1,8 +1,8 @@
-numero = float(input("Por favor, insira um número. "))
+numero =  float(input("Por favor, insira um número. "))
 
 if numero > 0:
-    print(f"O número é positivo.")
+    print(f"O número {numero} é positivo.")
 elif numero < 0:
-    print(f"O número é negativo.")
+    print(f"O número {numero} é negativo.")
 else:
-    print(f"O número é zero.")
+    print(f"O número inserido é zero.")
