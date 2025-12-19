175c133d9fed544a116b4e57b7b6fa3071dab0a0
meuNobre <nobrecoding@gmail.com>
2025-12-19 14:40:52 -0300

    Aula 15 Prova

---
+++ projetoAula15/database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Clientes (
+    ID INT PRIMARY KEY AUTO_INCREMENT, 
+    Nome VARCHAR(100) NOT NULL,         
+    Idade INT NOT NULL,                 
+    Cidade VARCHAR(100) NOT NULL       
+);
