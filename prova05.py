5b62b3d459eae9669befcfbd6a6aecb0e0adb677
Nobre <nobrecoding@gmail.com>
2025-12-20 22:27:17 -0300

    Python

---
+++ prova05.py
@@ -0,0 +1,15 @@
+def maior_numero(n1, n2, n3):
+    if n1 >= n2 and n1 >= n3:
+        return n1
+    elif n2 >= n1 and n2 >= n3:
+        return n2
+    else:
+        return n3
+
+# Exemplo de uso
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+maior = maior_numero(num1, num2, num3)
+print("O maior número é:", maior)
