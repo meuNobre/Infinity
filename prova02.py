87dee74e0f1ddaf1579f07b0438a5667c7176496
Nobre <nobrecoding@gmail.com>
2025-12-20 22:25:44 -0300

    Python

---
+++ prova02.py
@@ -0,0 +1,8 @@
+contato = {}
+
+contato["nome"] = input("Digite o nome do contato: ")
+contato["telefone"] = input("Digite o telefone do contato: ")
+contato["email"] = input("Digite o email do contato: ")
+
+print("\nInformações do contato:")
+print(contato)
