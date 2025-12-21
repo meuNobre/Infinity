ff541fa97156107d4e208170bbe64ca50d1083b6
Nobre <nobrecoding@gmail.com>
2025-12-20 22:26:37 -0300

    Python

---
+++ prova04.py
@@ -0,0 +1,10 @@
+def media(n1, n2, n3):
+    return (n1 + n2 + n3) / 3
+
+# Exemplo de uso
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+resultado = media(num1, num2, num3)
+print("A média é:", resultado)
